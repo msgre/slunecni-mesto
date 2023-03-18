3387f04f50abb2aee693c5b98887c56320206544
Michal Valousek <michal.valousek@kiwi.com>
2023-03-18 18:34:12 +0100

    Vice podrobnosti z Tesla shopu

--- scrappers/mesto/mesto/spiders/tesla.py
+++ scrappers/mesto/mesto/spiders/tesla.py
@@ -14,7 +14,7 @@ class TeslaSpider(scrapy.Spider):
     def __init__(self, market='CZ', language='cs', trim='SRRWD', paint='WHITE', interior='PREMIUM_WHITE', wheels='NINETEEN', *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.market = 'CZ'
-        params = {
+        self.params = {
             "query":{
                 "model":"my",
                 "condition":"new",
@@ -39,12 +39,27 @@ class TeslaSpider(scrapy.Spider):
             "outsideOffset":0,
             "outsideSearch":False
         }
-        query = quote(json.dumps(params))
+        query = quote(json.dumps(self.params))
         self.start_urls = [f'https://www.tesla.com/inventory/api/v1/inventory-results?query={query}']
 
     def parse(self, response):
+        data = response.json()
+        approx_results = [{
+            'TRIM': i['TRIM'],
+            'PAINT': i['PAINT'],
+            'INTERIOR': i['INTERIOR'],
+            'WHEELS': i['WHEELS'],
+            'ADL_OPTS': i['ADL_OPTS'],
+            'TotalPrice': i['TotalPrice'],
+            'City': i['City'],
+            'CurrencyCode': i['CurrencyCode'],
+            'EtaToCurrent': i['EtaToCurrent'],
+            'Year': i['Year'],
+        } for i in data['results']['approximate']]
         return {
-            "count": response.json()["total_matches_found"],
-            "market": self.market,
+            "count": data["total_matches_found"],
+            "approx_count": len(data['results']['approximate']),
+            "approx_results": approx_results,
+            "options": self.params['query']['options'],
             "datetime": datetime.now(),
         }
